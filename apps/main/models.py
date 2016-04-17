856e4e40d67973f8a9dc93391f00105a11be6045
Nikita 'CryptoManiac' Sivakov <cryptomaniac.512@gmail.com>
2016-04-17 16:37:23 +1000

    Добавлена модель услуг

--- apps/main/models.py
+++ apps/main/models.py
@@ -1,3 +1,5 @@
+from django.db import models
+
 from apps.core.models import BaseContentModel
 
 # Create your models here.
@@ -7,3 +9,14 @@ class Feature(BaseContentModel):
     class Meta:
         verbose_name = 'особенность'
         verbose_name_plural = 'особенности'
+
+
+class Services(BaseContentModel):
+    short_description = models.TextField(
+        'Краткое описание',
+        help_text='Для главной страницы и SEO'
+    )
+
+    class Meta:
+        verbose_name = 'услуга'
+        verbose_name_plural = 'услуги'
