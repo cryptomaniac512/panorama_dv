c69e9467409b1cf16a06d7e346bb24e820c7865d
Nikita 'CryptoManiac' Sivakov <cryptomaniac.512@gmail.com>
2016-04-17 19:31:54 +1000

    Обработка формы обратной связи для главной страницы

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,11 +1,24 @@
+from django.core.urlresolvers import reverse
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
+from .forms import FeedbackForm
 from .models import Features, Services
 
 
 def get_main_page(request):
-    features = Features.objects.all()
-    services = Services.objects.all()
-    return render(request, 'main/main.html', {
-        'features': features,
-        'services': services,
-    })
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            form.save()
+            form.send_mail(is_saved=True)
+            return HttpResponseRedirect(reverse('main:main'))
+    else:
+        features = Features.objects.all()
+        services = Services.objects.all()
+        form = FeedbackForm()
+        return render(request, 'main/main.html', {
+            'features': features,
+            'services': services,
+            'form': form,
+        })
