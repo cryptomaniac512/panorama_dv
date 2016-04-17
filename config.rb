1596e26ad08d071cbe100dcac9ff1d504edd7579
Nikita 'CryptoManiac' Sivakov <cryptomaniac.512@gmail.com>
2016-04-18 00:38:05 +1000

    TODO про 'избавиться от compass'

--- config.rb
+++ config.rb
@@ -1,5 +1,7 @@
 require 'compass/import-once/activate'
 
 # TODO: Раскурить, как указать здесь несколько директорий
+# TODO: Лучше бы заменить это на что-то питоническое и интегрировать
+# в приложение каким-то образом http://pyscss.readthedocs.org/en/latest/
 sass_dir = "apps/main/scss"
 css_dir = "apps/main/static/css"
