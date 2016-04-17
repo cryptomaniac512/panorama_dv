2aae944546193cee178caae1826ef089fb1c1dfa
Nikita 'CryptoManiac' Sivakov <cryptomaniac.512@gmail.com>
2016-04-17 16:33:09 +1000

    Использовать базовую модель контента

--- apps/main/models.py
+++ apps/main/models.py
@@ -1,15 +1,9 @@
-from django.db import models
+from apps.core.models import BaseContentModel
 
 # Create your models here.
 
 
-class Feature(models.Model):
-    title = models.CharField('Заголовок', max_length=100)
-    description = models.TextField('Описание')
-
-    def __str__(self):
-        return self.title
-
+class Feature(BaseContentModel):
     class Meta:
         verbose_name = 'особенность'
         verbose_name_plural = 'особенности'
