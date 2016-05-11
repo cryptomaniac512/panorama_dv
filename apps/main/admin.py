4dd71ceb444a6c26c6bd28a036ffd0a04b0ca13e
Nikita 'CryptoManiac' Sivakov <cryptomaniac.512@gmail.com>
2016-05-12 00:36:56 +1000

    Зарегистрировал модели в админку

---
+++ apps/main/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from main.models import Features, Feedback, Services
+
+# Register your models here.
+
+
+admin.site.register(Services)
+admin.site.register(Features)
+admin.site.register(Feedback)
