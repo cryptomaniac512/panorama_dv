ceec2eaad880c8ca1595b406f6443d149b819a97
Nikita 'CryptoManiac' Sivakov <cryptomaniac.512@gmail.com>
2016-07-12 17:01:28 +1000

    Форма с кастомным полем загрузки файла

---
+++ panoramas/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+
+from .models import PanoramaStore
+
+
+class PanoramaAdminForm(forms.ModelForm):
+
+    zip_file_field = forms.FileField(label='Архив для обработки',
+                                     required=False)
+
+    def save(self, commit=True):
+        zip_file_field = self.cleaned_data.get('zip_file_field', None)
+        return super(PanoramaAdminForm, self).save(commit=commit)
+
+    class Meta:
+        model = PanoramaStore
+        fields = '__all__'
