2fd15718484a83588d16545c8c15a3ea8bab903b
Nikita 'CryptoManiac' Sivakov <cryptomaniac.512@gmail.com>
2016-07-12 20:25:48 +1000

    Кастомная форма для портфолио

--- main/admin.py
+++ main/admin.py
@@ -4,18 +4,20 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .forms import ServicesAdminForm
+from .forms import ServicesAdminForm, PortfolioAdminForm
 from .models import Features, Feedback, Services, Portfolio
 
 
 @admin.register(Portfolio)
 class PortfoliAdmin(admin.ModelAdmin):
 
+    form = PortfolioAdminForm
     prepopulated_fields = {'slug': ('title', )}
 
 
 @admin.register(Services)
 class ServicesAdmin(admin.ModelAdmin):
+
     form = ServicesAdminForm
 
 admin.site.register(Features)
