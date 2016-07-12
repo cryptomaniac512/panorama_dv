352cb3c5fe7c264c6891e1e34381d5c8da009b60
Nikita 'CryptoManiac' Sivakov <cryptomaniac.512@gmail.com>
2016-07-12 18:49:33 +1000

    View страницы услуг

--- main/views.py
+++ main/views.py
@@ -2,8 +2,8 @@ from django.core.urlresolvers import reverse
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
-from main.forms import FeedbackForm
-from main.models import Features, Services
+from .forms import FeedbackForm
+from .models import Features, Services, Portfolio
 
 
 def get_main_page(request):
@@ -19,6 +19,26 @@ def get_main_page(request):
         form = FeedbackForm()
         return render(request, 'main/main.html', {
             'features': features,
+            'services': services,
+            'form': form,
+        })
+
+
+def get_services_page(request):
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            form.save()
+            form.send_mail(is_saved=True)
+            return HttpResponseRedirect(reverse('main:services'))
+    else:
+        portfolio = Portfolio.objects.filter(
+            is_published=True, on_services=True
+        ).order_by('-pub_date')[:4]
+        services = Services.objects.all()
+        form = FeedbackForm()
+        return render(request, 'website/content_pages/services.html', {
+            'portfolio': portfolio,
             'services': services,
             'form': form,
         })
