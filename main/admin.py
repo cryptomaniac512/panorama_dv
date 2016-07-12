4d97e4c0d4372aa7aa133ada50b58ad2608dec34
Nikita 'CryptoManiac' Sivakov <cryptomaniac.512@gmail.com>
2016-07-12 19:03:39 +1000

    Автоматически генерировать URL

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from main.models import Features, Feedback, Services
+from main.models import Features, Feedback, Services, Portfolio
 
+
+@admin.register(Portfolio)
+class PortfoliAdmin(admin.ModelAdmin):
+
+    prepopulated_fields = {'slug': ('title', )}
 
 admin.site.register(Services)
 admin.site.register(Features)
