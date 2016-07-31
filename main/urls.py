d5eb153875eccf8f9cdc83dc5e468076a21ac409
Nikita 'CryptoManiac' Sivakov <cryptomaniac.512@gmail.com>
2016-08-01 02:04:29 +1000

    Правила url для портфолио

--- main/urls.py
+++ main/urls.py
@@ -7,5 +7,8 @@ app_name = 'main'
 urlpatterns = [
     url(r'^$', views.get_main_page, name='main'),
     url(r'^services/$', views.get_services_page, name='services'),
+    url(r'^portfolios/$', views.get_portfolios_page, name='portfolios'),
+    url(r'^portfolio/(?P<pk>[0-9]+)/$', views.get_portfolio_page,
+        name='portfolio'),
     url(r'^feedback/$', views.feedback_submit, name='feedback'),
 ]
