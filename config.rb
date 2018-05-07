e1334d72173189214c4668deb621a8eeeece5d52
Nikita Sivakov <sivakov512@gmail.com>
2018-05-08 01:07:25 +1000

    Manually add required static

--- config.rb
+++
@@ -1,7 +0,0 @@
-require 'compass/import-once/activate'
-
-# TODO: Раскурить, как указать здесь несколько директорий
-# TODO: Лучше бы заменить это на что-то питоническое и интегрировать
-# в приложение каким-то образом http://pyscss.readthedocs.org/en/latest/
-sass_dir = "main/scss"
-css_dir = "main/static/css"
